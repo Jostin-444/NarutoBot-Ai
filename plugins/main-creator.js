8d58d93b057300f4d21a20eee718786ca7a749c6
Jostin-444 <gemersport7@gmail.com>
2024-09-29 10:18:05 -0600

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let txt_owner = "> _*`Hola, Este es el numero de mi creadxr, cualquier falla o si quieres agregar el bot a tu grupo, puedes hablarle`*_\n\n `+54 9 381 645-7465`"
+let txt_owner = "> _*`Hola, Este es el numero de mi creadxr, cualquier falla o si quieres agregar el bot a tu grupo, puedes hablarle`*_\n\n `+502 4207 3893`"
 await conn.sendFile(m.chat, "https://freeimage.host/i/dZ1A2Yg", 'thumbnail.jpg', txt_owner, m, null, rcanal)
 }
 handler.help = ['owner']
