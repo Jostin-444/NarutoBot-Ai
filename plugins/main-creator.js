a4ffc4ad5cd81fb22d27c15fbadc51cabb88fb0f
Jostin-444 <gemersport7@gmail.com>
2024-10-04 23:23:42 -0600

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
 let txt_owner = "> _*`𝙷𝙾𝙻𝙰, 𝙴𝚂𝚃𝙴 𝙴𝚂 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝙼𝙸 𝙲𝚁𝙴𝙰𝙳𝙾𝚁, 𝙲𝚄𝙰𝙻𝚀𝚄𝙸𝙴𝚁 𝙵𝙰𝙻𝙻𝙰 𝙾 𝚂𝙸 𝚀𝚄𝙸𝙴𝚁𝙴𝚂 𝙰𝙶𝚁𝙴𝙶𝙰𝚁 𝙴𝙻 𝙱𝙾𝚃 𝙰 𝚃𝚄 𝙶𝚁𝚄𝙿𝙾, 𝙿𝚄𝙴𝙳𝙴𝚂 𝙷𝙰𝙱𝙻𝙰𝚁𝙻𝙴`*_\n\n ム 𝐉𝐎𝐒𝐓𝐈𝐍-444 ㋰ : +502 4207 3893"
-await conn.sendFile(m.chat, "https://f.uguu.se/DawLyPan.jpg", 'thumbnail.jpg', txt_owner, m, null, rcanal)
+await conn.sendFile(m.chat, "https://i.ibb.co/1bhcmQg/Sylph.jpg", 'thumbnail.jpg', txt_owner, m, null, rcanal)
 }
 handler.help = ['owner']
 handler.tags = ['main']
