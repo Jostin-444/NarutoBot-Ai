488774e575ffda5533dd035fb3f6daea9d8235c3
Jostin-444 <gemersport7@gmail.com>
2024-10-04 23:07:19 -0600

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -20,12 +20,12 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 		} else if (/video/g.test(mime)) {
 			if ((q.msg || q).seconds > 10) throw 'Maximo 10 seconds!'
 			let img = await q.download?.()
-conn.reply(m.chat, wait, m, rcanal)
+			//conn.reply(m.chat, wait, m, rcanal)
 			stiker = await mp4ToWebp(img, { pack: global.packname, author: global.author })
 		} else if (args[0] && isUrl(args[0])) {
 			//m.reply(wait)
 			stiker = await createSticker(false, args[0], '', global.packname, 20)
-		} else throw m.conn(m.chat, wait, m, rcanal, `🚩 Responda a una *Imagen* o *Vídeo.*`, m, rcanal)
+		} else throw m.reply(`🚩 Responda a una *Imagen* o *Vídeo.*`)
 	} catch (e) {
 		console.log(e)
 		stiker = e
