bfe4c5bcef046df3cee1a5af3429c9f39ec6cdc5
Jostin-444 <gemersport7@gmail.com>
2024-10-05 22:36:51 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -206,7 +206,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
 
 handler.help = ['allmenu']
 handler.tags = ['main']
-handler.command = ['menu2', 'help2', 'menú2'] 
+handler.command = ['menu2', 'help2', 'menú2', 'allmenu'] 
 handler.register = true 
 export default handler
 
