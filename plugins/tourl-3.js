7170f03209f9645e787f6d38999415c69777c124
Jostin-444 <gemersport7@gmail.com>
2024-10-08 22:17:33 -0600

    Actualizar plugins

---
+++ plugins/tourl-3.js
@@ -0,0 +1,21 @@
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+
+let handler = async (m) => {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'يرجى الرد على صورة/فيديو'
+  let media = await q.download()
+  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+  let link = await (isTele ? uploadImage : uploadFile)(media)
+  m.reply(`▢ ${media.length} بايت
+
+▢ ${isTele ? '(بدون تاريخ انتهاء)' : '(غير معروف)'}
+▢ *رابط :* ${link}
+  `)
+}
+handler.help = ['tourl']
+handler.tags = ['tools']
+handler.command = ['upload', 'tourl','لرابط']
+
+export default handler
