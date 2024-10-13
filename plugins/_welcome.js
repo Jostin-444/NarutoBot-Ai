37eef0e14e0eb721380e1e30e305e8b860e0e024
Jostin-444 <gemersport7@gmail.com>
2024-10-13 15:17:00 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,4 +1,4 @@
-import {WAMessageStubType} from '@whiskeysockets/baileys'
+import {WAMessageStubType} from '@adiwajshing/baileys'
 import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
