53879267728c79a8aa14f7cc63c66410f3f26b1c
Jostin-444 <gemersport7@gmail.com>
2024-10-13 18:44:39 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let require = createRequire(__dirname)
 let { say } = cfonts
 let rl = createInterface(process.stdin, process.stdout)
 
-say('Samatoki Ai', {
+say('NarutoBot-Ai', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
