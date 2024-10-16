cf2bce9d6dd4d67c26f8396bf8a1c798f9c111c5
Jostin-444 <gemersport7@gmail.com>
2024-10-15 20:14:25 -0600

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,20 +1,24 @@
 const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
+
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
+m.react('🍿') 
   if (!(isAdmin || isOwner)) {
     global.dfail('admin', m, conn);
-    return;
+    throw false;
   }
   const pesan = args.join` `;
-  const colombia = `💌 *Mensaje:* ${pesan}`;
-  let teks = `💥 *INVOCANDO GRUPO*\n${colombia}\n\n🔥 *Tags:*\n`;
+const oi = `⇢=͟͟͞͞🄰νίऽ૭ : ${pesan}`;
+  let teks = `(づ｡◕‿◕｡)づ мєη¢ιóη gєηєяαℓ "\n  ⧼P̼⧽= ${participants.length} ℙ𝐀𝔍𝐈ꪀəﻜ\n\n ${oi}\n\n╭•┈┈•┈┈⊰🍿⃪᜔꫶┈•┈┈•◌ᜓ ݊ ᜒ𝅄\n`;
   for (const mem of participants) {
-    teks += `@${mem.id.split('@')[0]}\n`;
+    teks += `│ ❏ᝰ. @${mem.id.split('@')[0]}\n`;
   }
+  teks += `╰─┐ • •ㅤ•-ˏˋ✿ˊˎ-• •ㅤ•
+        ꒷︶︶꒷︶︶꒷꒦︶✧꒷₊˚`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
-handler.help = ['tagall *<mesaje>*', 'invocar *<mesaje>*'];
+handler.help = ['todos <mesaje>'];
 handler.tags = ['grupo'];
-handler.command = ['tagall', 'invocar', 'todos'];
+handler.command = /^(tagall|invocar|marcar|todos|invocación)$/i;
 handler.admin = true;
 handler.group = true;
 export default handler;
